bb6a332781a3fa02bdc07592d80b82a28b92f2c6
Noah <Nphilli7@students.tulsatech.edu>
2024-10-08 13:59:10 -0500

    Done

--- Spinner/ContentView.swift
+++ Spinner/ContentView.swift
@@ -13,47 +13,125 @@ struct ContentView: View {
         
         let rotation: Angle
         let isCurrent: Bool
+        let isCompleted: Bool
         
         var body: some View {
             Capsule()
-                .stroke(isCurrent ? Color.white : Color.gray, lineWidth: 8)
-                .frame(width: 20, height: 50)
-                .offset(x: 0, y: 70)
+                .stroke(isCurrent ? Color.white : Color.gray, lineWidth: 10)
+//                .frame(width: 20, height: 50)
+            // initial offset
+//                .offset(x: 0, y: 70)
+            
+            // item will fold in
+                .frame(width: 20, height: isCompleted ? 20 : 50)
+            
+            // conditional offset based on isCurrent
+                .offset(
+                    isCurrent
+                    ? .init(width: 10, height: 0)
+                    : .init(width: 40, height: 70)
+            )
+            
+            // makes leafs smaller
+                .scaleEffect(isCurrent ? 0.5 : 1)
             
             // applies the calculated rotation angle to the view
-                .rotationEffect(rotation)
+//                .rotationEffect(rotation)
+            
+            // item will fold in on itself
+                .rotationEffect(isCompleted ? .zero : rotation)
+                .animation(.easeInOut(duration: 1.5), value: isCompleted)
         }
     }
-    // ignore these comments. Bulk and multiplayer sell system
-    // plate+re execute at @a [r=1 , hasitem={item=diamond , quantity=2..}] run scoreboard add @p Money 20
-    // c+aa clear @a [r=2 , hasitem={item=diamond , quantity=2..}] diamond 0 2
-    //
+    // ignore these comments
     // Npc tp to private storage using id system. ? = Int. Copy and add to new line with enumerated Id
     // execute as @initiator if entity @s [scores={Id=?}] run tp @s ? ? ?
     
     
     @State var currentIndex = -1
+    @State var completed = false
+    @State var isVisible = true
+    @State var currentOffset = CGSize.zero
+    
+    // transition that will move item upwards and off the screen
+    let shootUp = AnyTransition.offset(x: 0, y: -1000)
+        .animation(.easeIn(duration: 1))
     
     var body: some View {
         VStack {
-            ZStack {
-                // renders 12 instances of the leaf
-                ForEach(0..<12) { index in
-                    // calculates rotation angle based on index position
-                    Leaf(rotation: .init(degrees: .init(index) / .init(12) * 360),
-                isCurrent: index == currentIndex
-                         )
+            
+            
+            
+            if isVisible {
+                
+                
+                
+                
+                ZStack {
+                    // renders 12 instances of the leaf
+                    ForEach(0..<12) { index in
+                        // calculates rotation angle based on index position
+                        Leaf(rotation: .init(degrees: .init(index) / .init(12) * 360),
+                             isCurrent: index == currentIndex,
+                             isCompleted: completed
+                        )
+                    }
                 }
+                .offset(currentOffset)
+                
+                .blur(radius: currentOffset == .zero ? 0 : 10)
+                
+                .animation(.easeIn(duration: 1), value: currentOffset)
+                
+                .gesture(DragGesture()
+                    .onChanged { gesture in
+                        currentOffset = gesture.translation
+                    }
+                    .onEnded{ gesture in
+                        if currentOffset.height > 150 {
+                            isCompleted()
+                        }
+                        currentOffset = .zero
+                    }
+                )
+                         
+                .transition(shootUp)
+                // initiates animation when the view appears
+                .onAppear(perform: animate)
             }
-            // initiates animation when the view appears
-            .onAppear(perform: animate)
+        }
+    }
+    
+    func isCompleted() {
+        guard !completed else { return }
+        
+        completed = true
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
+            withAnimation {
+                isVisible = false
+            }
         }
     }
     
     // animates the leaf x12 in a loop. Initiates an animation loop
     func animate() {
+        
+        var iteration = 0
+        
         Timer.scheduledTimer(withTimeInterval: 0.15, repeats: true) { timer in
             currentIndex = (currentIndex + 1) % 12
+            iteration += 1
+            
+            // animation runs 30 times
+            if iteration == 36 {
+                timer.invalidate()
+                isCompleted()
+                completed = true
+                
+                // circle fades away after 2 seconds
+                
+            }
         }
     }
 }
